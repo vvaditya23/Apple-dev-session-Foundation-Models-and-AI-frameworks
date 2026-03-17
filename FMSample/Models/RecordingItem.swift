2743cb1fb48240aa29f407fefb199abbe421476f
Aditya Vyavahare <vvaditya23@gmail.com>
2026-03-17 15:46:15 +0530

    Generate image from user speech

--- FMSample/Models/RecordingItem.swift
+++ FMSample/Models/RecordingItem.swift
@@ -8,6 +8,7 @@ Model class for meeting items that represent recordings and their transcription.
 import Foundation
 import CoreGraphics
 import FoundationModels
+import ImagePlayground
 
 @Observable
 class RecordingItem: MeetingItem {
@@ -40,7 +41,23 @@ class RecordingItem: MeetingItem {
         return answer.content.title.trimmingCharacters(in: .punctuationCharacters)
     }
 
+    @Generable
+    struct ImagePrompt {
+        @Guide(description: "A simple description of an image that can be generated based on the meeting notes")
+        var prompt: String
+    }
+    
     func suggestedImage() async throws -> CGImage? {
-        nil
+        let session = LanguageModelSession(model: SystemLanguageModel.default, instructions: "You are a helpful assistant that takes meeting notes. From those notes you MUST extract 3 or 4 terms that can be used to visualize key concepts in the notes by using Image Playground. You must only output a string that can be used by Image Playground to generate an image.")
+        
+        let answer = try await session.respond(to: String(text.characters), generating: ImagePrompt.self)
+        let concept = ImagePlaygroundConcept.extracted(from: answer.content.prompt)
+        let creator = try await ImageCreator()
+        let imageSequence = creator.images(for: [concept], style: .sketch, limit: 1)
+        
+        for try await image in imageSequence {
+            return image.cgImage
+        }
+        return nil
     }
 }
