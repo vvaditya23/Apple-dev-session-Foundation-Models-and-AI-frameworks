5479776320cb96e15f0f3bb8735283d44c76805b
Aditya Vyavahare <vvaditya23@gmail.com>
2026-03-17 17:33:45 +0530

    Add Q&A oriented to the provided text doc

--- FMSample/Views/QuestionAnswerSheet.swift
+++ FMSample/Views/QuestionAnswerSheet.swift
@@ -6,11 +6,14 @@ View that displays answers to user questions.
 */
 
 import SwiftUI
+import FoundationModels
 
 struct QuestionAnswerSheet: View {
     @Environment(\.dismiss) private var dismiss
     let meetingItems: [MeetingItem]
     let question: String
+
+    @State private var session: LanguageModelSession?
 
     @State private var answer: AttributedString = ""
     @State private var generationState: AnswerGenerationState = .started
@@ -51,7 +54,79 @@ struct QuestionAnswerSheet: View {
                     }
                 }
             }
+            .task {
+                if session == nil {
+                    let model = SystemLanguageModel(useCase: .general)
+                    session = LanguageModelSession(
+                        model: model,
+                        instructions: """
+                        You are a helpful office team member who takes meeting notes and collects documents related to the team's projects. Answer the user's question below, based ONLY on the provided information.
+                        Important: if the user's question is unrelated to the provided information, say "Sorry, I can't answer that question".
+                        """
+                    )
+                }
+                
+                do {
+                    try await fetchAnswer()
+                } catch let error as LanguageModelSession.GenerationError {
+                    switch error {
+                    case .guardrailViolation:
+                        print("Generation was blocked due to safety guardrails: \(error)")
+                    default:
+                        print("Generation error: \(error)")
+                    }
+                } catch {
+                    print("Error: \(error)")
+                }
+            }
         }
         .adaptiveSheetFrame()
+    }
+
+    @Generable
+    struct Response {
+        @Guide(description: "An exact quote containing the answer from the source material")
+        var citation: String
+
+        @Guide(description: "A brief answer to the question")
+        var answer: String
+
+        @Guide(description: "Whether it was not possible to answer the question")
+        var insufficinetInformation: Bool
+    }
+
+    private func fetchAnswer() async throws {
+        defer { generationState = .completed }
+        guard let session else { return }
+        
+        let text = meetingItems
+            .map { String($0.text.characters) }
+            .joined(separator: "\n\n")
+
+        // Prompt is passed in the closure
+        let stream = session.streamResponse(generating: Response.self) {
+            "Question \(question)"
+            "This is all the information that you can use to answer the question:"
+            text
+        }
+        
+        for try await partialAnswer in stream {
+            if partialAnswer.content.insufficinetInformation == true {
+                answer = "I couldn't find enough information to answer this"
+            } else {
+                var answerText = AttributedString(partialAnswer.content.answer ?? "")
+                
+                if let citation = partialAnswer.content.citation,
+                   !citation.isEmpty {
+                    var citationText = AttributedString("\n\nBecause “\(citation)”")
+                    citationText.font = .caption.italic()
+                    citationText.foregroundColor = .secondary
+                    answerText.append(citationText)
+                }
+
+                answer = answerText
+            }
+            generationState = .generating
+        }
     }
 }
