3106fa5cd7c683f0f4c33a05df3d48a8b89377b2
Aditya Vyavahare <vvaditya23@gmail.com>
2026-03-17 15:25:28 +0530

    Generate title from user speech

--- FMSample/Models/RecordingItem.swift
+++ FMSample/Models/RecordingItem.swift
@@ -7,6 +7,7 @@ Model class for meeting items that represent recordings and their transcription.
 
 import Foundation
 import CoreGraphics
+import FoundationModels
 
 @Observable
 class RecordingItem: MeetingItem {
@@ -20,8 +21,23 @@ class RecordingItem: MeetingItem {
         RecordingItem(title: "New Recording", text: "")
     }
 
+    @Generable
+    struct RecordingTitle {
+        var title: String
+    }
+
     func suggestedTitle() async throws -> String? {
-        nil
+        // Note: Capitalization matters (all caps == emphasis)
+        let session = LanguageModelSession(
+            model: SystemLanguageModel.default,
+            instructions: "You are an expert headline writer who takes meeting notes. From those notes you MUST generate most relevant title, with no other text."
+        )
+
+        let answer = try await session.respond(
+            to: String(text.characters),
+            generating: RecordingTitle.self
+        )
+        return answer.content.title.trimmingCharacters(in: .punctuationCharacters)
     }
 
     func suggestedImage() async throws -> CGImage? {
